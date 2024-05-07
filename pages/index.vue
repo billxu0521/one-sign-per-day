67ec3b7f4c24eaabf789cde8bef8179510ee5769
billxu <billxu0521@gmail.com>
2024-05-07 14:42:08 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,15 @@
     <div class="flex flex-col text-center text-3xl font-light">
       <div><p>{{randomSign?.en}}</p></div>
       <div><p>{{randomSign?.zhtw}}</p></div>
+      <div @click="getRandomSign" class="m-auto">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="glow-icon">
+          <path d="M13.5 2c-5.288 0-9.649 3.914-10.377 9h-3.123l4 5.917 4-5.917h-2.847c.711-3.972 4.174-7 8.347-7 4.687 0 8.5 3.813 8.5 8.5s-3.813 8.5-8.5 8.5c-3.015 0-5.662-1.583-7.171-3.957l-1.2 1.775c1.916 2.536 4.948 4.182 8.371 4.182 5.797 0 10.5-4.702 10.5-10.5s-4.703-10.5-10.5-10.5z"/>
+        </svg>
+      </div>
     </div>
+    
   </div>
+  
   <div class="wave"></div>
   <div class="wave"></div>
   <div class="wave"></div>
@@ -23,8 +30,13 @@ useHead({
 
 const signList = useSignList();
 const randomSign = ref(null);
-randomSign.value = getRandomSignData(signList?.signListData);
 
+const getRandomSign = () => {
+  randomSign.value = getRandomSignData(signList?.signListData);
+};
+onMounted(() => {
+  getRandomSign();
+});
 </script>
 
 <style>
